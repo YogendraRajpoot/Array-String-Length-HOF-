fd0f3252af14c10d97099f82dbeb05de13123cf8
YogendraRajpoot <yogendrarajpooten1030@gmail.com>
2022-01-14 09:31:34 +0530

    first commit

---
+++ ASL.js
@@ -0,0 +1,5 @@
+// Given an array of strings print the length of each string (map)
+
+const arr = ["desk" ,"chair" , "book" , "beaker" ,"pencil" , "calculator" , "clock" , "blackboard"]
+ const arrayString = x => x.length
+console.log(arr.map(arrayString))
